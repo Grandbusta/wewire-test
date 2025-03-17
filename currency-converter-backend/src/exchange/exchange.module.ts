19e97cf993a7e70ff887199964464e08ceb904dd
grandbusta <bustajay30@gmail.com>
2025-03-17 04:06:36 +0100

    Implement cache in exchange rates

--- currency-converter-backend/src/exchange/exchange.module.ts
+++ currency-converter-backend/src/exchange/exchange.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { HttpModule } from '@nestjs/axios';
 import { ExchangeService } from './exchange.service';
 import { ExchangeController } from './exchange.controller';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register()],
   providers: [ExchangeService],
   controllers: [ExchangeController],
   exports: [ExchangeService],
 })
-export class ExchangeModule {}
+export class ExchangeModule { }
--- currency-converter-backend/src/exchange/exchange.service.ts
+++ currency-converter-backend/src/exchange/exchange.service.ts
@@ -1,19 +1,30 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Inject } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { SUPPORTED_CURRENCIES } from '../constants/supported-currencies';
 import { lastValueFrom } from 'rxjs';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 
 @Injectable()
 export class ExchangeService {
-    constructor(private readonly httpService: HttpService) { }
-    
+    constructor(
+        private readonly httpService: HttpService,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
+    ) { }
+
     getSupportedCurrencies(): string[] {
         return SUPPORTED_CURRENCIES;
     }
 
 
     async getLatestRates(): Promise<any> {
+        const cacheKey = 'exchange-rates';
+        const cachedValue = await this.cacheManager.get(cacheKey);
+        if (cachedValue) {
+            console.log('------Cache hit!');
+            return cachedValue;
+        }
         const appId = process.env.OPEN_EXCHANGE_RATES_APP_ID;
         if (!appId) {
             throw new Error('Open Exchange Rates API key is not configured');
@@ -32,7 +43,11 @@ export class ExchangeService {
               return acc;
             }, {});
       
-            return { base: "USD", rates: filteredRates };
+            const data = { base: "USD", rates: filteredRates };
+            
+            await this.cacheManager.set(cacheKey, data, 3600000); // Cache for 1 hour
+
+            return data;
           } catch (error) {
             throw new HttpException('Failed to fetch exchange rates', HttpStatus.BAD_GATEWAY);
           }
